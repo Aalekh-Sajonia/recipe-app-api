2d8dffc9331dd4a743a1569a4d15b7351497c079
Aalekh-Sajonia <aalekhbh@gmail.com>
2021-11-07 22:48:59 +0530

    added user update api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse("user:create")
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
@@ -86,3 +87,29 @@ class PublicUserApiTest(TestCase):
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         
+    def test_retrive_user_unauth(self):
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code , status.HTTP_401_UNAUTHORIZED)
+
+class PrivateUserApiTests(TestCase):
+
+    def setUp(self):
+        self.user = create_user(
+            email = "test@test.com",
+            password = "123456678",
+            name = "test"
+        )
+
+        self.client = APIClient()
+        self.client.force_authenticate(user = self.user)
+
+    def test_retrieve_profile_success(self):
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    
