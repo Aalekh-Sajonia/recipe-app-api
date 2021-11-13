00285e1d3ace2dbcdd2101ec44ac4b35f35ac34e
Aalekh-Sajonia <aalekhbh@gmail.com>
2021-11-13 21:42:02 +0530

    added recipes endpoints

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -56,3 +56,11 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        recipe = models.Recipe.objects.create(
+            user = sample_user(),
+            title = "Steak and mushroom sauce",
+            time_minutes = 5,
+            price = 5.00
+        )
